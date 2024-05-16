cb8f7ae186ac3d0e81a139a52787e00e46ef9792
ali yuldashev <aliyuldashev880@gmail.com>
2024-05-16 22:19:49 +0900

    fixed the issue

--- week 10/tree.cpp
+++ week 10/tree.cpp
@@ -1,54 +1,69 @@
 #include <iostream>
-using namespace  std;
+using namespace std;
 
-class Node{
+class Node {
 public:
-    Node(string value){
-        this->value = value;
-    }
-
+    Node(string value) : value(value), left(nullptr), right(nullptr) {}
 
     string value;
-    Node* left ;
+    Node* left;
     Node* right;
 };
-void inorder(Node* node){
-    if(node == nullptr){
-        return ;
+
+void inorder(Node* node) {
+    if (node == nullptr) {
+        return;
     }
     inorder(node->left);
     cout << node->value << " ";
     inorder(node->right);
-
-
 }
-void preorder(Node* node){
-    if(node == nullptr){
-        return ;
+
+void preorder(Node* node) {
+    if (node == nullptr) {
+        return;
     }
-    preorder(node->right);
-    preorder(node->left);
     cout << node->value << " ";
-}
-void postorder(Node* node){
-    if(node == nullptr){
-        return ;
-    }
     preorder(node->left);
     preorder(node->right);
+}
+
+void postorder(Node* node) {
+    if (node == nullptr) {
+        return;
+    }
+    postorder(node->left);
+    postorder(node->right);
     cout << node->value << " ";
 }
-Node settree( ){
+
+Node* settree() {
     Node* root = new Node("J");
     root->left = new Node("F");
     root->left->left = new Node("D");
     root->left->left->left = new Node("C");
-
-
+    root->left->left->right = new Node("E");
+    root->left->right = new Node("G");
+    root->left->right->right = new Node("I");
+    root->right = new Node("K");
+    root->right->right = new Node("L");
+    return root;
 }
 
-int main(){
+int main() {
+    Node* root = settree();
 
+    cout << "Inorder traversal: ";
+    inorder(root);
+    cout << endl;
+
+    cout << "Preorder traversal: ";
+    preorder(root);
+    cout << endl;
+
+    cout << "Postorder traversal: ";
+    postorder(root);
+    cout << endl;
 
     return 0;
 }
